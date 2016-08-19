9e331ab947e60ad03f85fb30d46a30ccae5985c8
lixihua <lixihua9@126.com>
2016-08-19 11:26:36 +0800

    viewer bug need fixed

--- Examples/Monocular/mono_tum.cc
+++ Examples/Monocular/mono_tum.cc
@@ -51,7 +51,10 @@ int main(int argc, char **argv)
     int nImages = vstrImageFilenames.size();
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
+    // argv[1] == Vocabulary/ORBvoc.txt: trained out from a huge database, which is more or less a kind of generic
+    // argv[2] == Examples/Monocular/TUMX.yaml: parameters and camera parameter
+    ORB_SLAM2::System SLAM(argv[1], argv[2], ORB_SLAM2::System::MONOCULAR, true);
+    //ORB_SLAM2::System SLAM(argv[1], argv[2], ORB_SLAM2::System::MONOCULAR, false);
 
     // Vector for tracking time statistics
     vector<float> vTimesTrack;
@@ -61,20 +64,27 @@ int main(int argc, char **argv)
     cout << "Start processing sequence ..." << endl;
     cout << "Images in the sequence: " << nImages << endl << endl;
 
+    // Image showing window
+    //cv::namedWindow("Slam_Show", CV_WINDOW_AUTOSIZE);
+
     // Main loop
     cv::Mat im;
-    for(int ni=0; ni<nImages; ni++)
+    //cv::Mat out_im;
+    for (int ni = 0; ni < nImages; ni++)
     {
         // Read image from file
-        im = cv::imread(string(argv[3])+"/"+vstrImageFilenames[ni],CV_LOAD_IMAGE_UNCHANGED);
+        im = cv::imread(string(argv[3])+"/"+vstrImageFilenames[ni], CV_LOAD_IMAGE_UNCHANGED);
         double tframe = vTimestamps[ni];
 
-        if(im.empty())
+        if (im.empty())
         {
             cerr << endl << "Failed to load image at: "
                  << string(argv[3]) << "/" << vstrImageFilenames[ni] << endl;
             return 1;
         }
+
+        //cv::imshow("Slam_Show", im);
+        //cv::waitKey(10);
 
 #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
@@ -83,31 +93,33 @@ int main(int argc, char **argv)
 #endif
 
         // Pass the image to the SLAM system
-        SLAM.TrackMonocular(im,tframe);
-
+        SLAM.TrackMonocular(im, tframe);
+        //cv::imshow("Slam_Show", im);
+        //cv::waitKey(10);
 #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
 #else
         std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
 #endif
 
-        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
-
-        vTimesTrack[ni]=ttrack;
+        double ttrack = std::chrono::duration_cast < std::chrono::duration<double> >(t2 - t1).count();
+        cout << ni << ":" << ttrack << endl;
+        vTimesTrack[ni] = ttrack;
 
         // Wait to load the next frame
-        double T=0;
-        if(ni<nImages-1)
+        double T = 0;
+        if (ni < nImages-1)
             T = vTimestamps[ni+1]-tframe;
-        else if(ni>0)
+        else if(ni > 0)
             T = tframe-vTimestamps[ni-1];
 
-        if(ttrack<T)
+        if (ttrack<T)
             usleep((T-ttrack)*1e6);
     }
 
     // Stop all threads
     SLAM.Shutdown();
+    //cv::destroyWindow("Slam_Show");
 
     // Tracking time statistics
     sort(vTimesTrack.begin(),vTimesTrack.end());
@@ -146,9 +158,9 @@ void LoadImages(const string &strFile, vector<string> &vstrImageFilenames, vecto
             stringstream ss;
             ss << s;
             double t;
-            string sRGB;
             ss >> t;
             vTimestamps.push_back(t);
+            string sRGB;
             ss >> sRGB;
             vstrImageFilenames.push_back(sRGB);
         }
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 PATH_TO_SEQUENCE=/Users/lixihua/Downloads/database/rgbd_dataset_frei/rgbd_dataset_freiburg1_desk
 
-# [Testing???]06.RGB-D example: TUM Dataset
+# [Testing!!!]06.RGB-D example: TUM Dataset
 python associate.py $PATH_TO_SEQUENCE/rgb.txt $PATH_TO_SEQUENCE/depth.txt > associations.txt
 ./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml "$PATH_TO_SEQUENCE" associations.txt
 
